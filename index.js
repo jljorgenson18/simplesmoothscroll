c544dbf02dd2c6478cfa8aa84a3d7d86eb4ce3a7
jljorgenson18 <jljorgenson18@gmail.com>
2016-04-05 21:29:21 -0500

    Did a major refactor and simplified everything. Added appropriate comments, added the passed-bottom-difference fix, added more divs to the example

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 
 (function() {
 
+    /**
+     * There are a few descriptions of some basic dom and window
+     * attributes and methods in case you get confused
+     *
+     * window.pageYOffset: an alias for window.scrollY which is
+     * the number of pixels that the document is currently
+     * scrolled from the top.
+     *
+     * getBoundingClientRect: method returns the size of an element
+     * and its position relative to the viewport...top and left are
+     * relative to the top-left of the viewport.
+     *
+     **/
+
     var getHeightOfEntirePage = function() {
         var body = document.body;
         var html = document.documentElement;
@@ -14,75 +28,107 @@
         return ("innerHeight" in window) ? window.innerHeight : document.documentElement.offsetHeight;
     };
 
-    // Finds the Y-coordinate of your target.
-    // Defaults to top of the page
-    var getTargetPosition = function(ele, offset) {
+    // Returns 0 if it doesn't pass the bottom or
+    // the difference if scrolling down would
+    // pass the bottom of the page
+    var passedBottom = function(distFromTopOfViewport) {
+        var difference = 0;
+        // We can only pass the bottom if we are scrolling down
+        if (distFromTopOfViewport < 0) {
+            var pageHeight = getHeightOfEntirePage();
+            var windowHeight = getHeightOfWindow();
+            var endingPageYOffset = window.pageYOffset - distFromTopOfViewport;
+            if (endingPageYOffset + windowHeight > pageHeight) {
+                difference = endingPageYOffset + windowHeight - pageHeight;
+            }
+        }
+        return difference;
+    }
+
+    /**
+     * Finds the Y-Coordinate relative to the viewport
+     * that we want to scrollTo.
+     * Negative values imply that the target is below the
+     * top of the viewport.
+     * Positive values imply that the
+     * target is above the top of the viewport
+     *
+     * Default: Scroll to the top of the page by
+     * finding how far we have already scrolled using
+     * window.pageYOffset
+     */
+    var getScrollTargetPosition = function(ele, offset) {
+        if (!ele) {
+            return window.pageYOffset;
+        }
         if (!offset) {
             offset = 0;
         }
-        if (ele) {
-            return -ele.getBoundingClientRect().top + offset;
-        } else {
-            return window.scrollY;
-        }
+        var distFromTopOfViewport = -ele.getBoundingClientRect().top;
+        offset += passedBottom(distFromTopOfViewport);
+        return distFromTopOfViewport + offset;
     };
 
-    // This is the duration
-    // The sqrt is to move the duration closer to 1000ms
-    var getScrollStep = function(targetPosition) {
-        return Math.PI / ((Math.sqrt(Math.abs(targetPosition) / 1000) * 1000) / 15);
+    // This returns a step size which essentially dictates the
+    // speed of the scroll. This math was done to make shorter scrolls
+    // go a little slower relative to the distance and longer scrolls
+    // go a little faster relative to the distance. This is essentially
+    // trial and error with some trig
+    var getStepSize = function(targetPosition) {
+        return (Math.PI * 15) / (Math.sqrt(Math.abs(targetPosition) / 1000) * 1000);
     };
 
-    var getScrollHeight = function() {
-        return window.pageYOffset;
-    };
+    // TODO: Make the easing function customizable
+    // Essentially what is going on here is that we want the easing function
+    // to be about 0 when the scrolling starts and about 0 when the scrolling
+    // finishes. We use some trigonometry to do that
+    var getScrollMargin = function(targetPosition, stepCount, stepSize) {
+        return (targetPosition / 2) * (1 - Math.cos(stepCount * stepSize));
+    }
 
     var smoothScroll = function(ele, offset) {
-        var targetPosition = getTargetPosition(ele, offset);
-        var scrollStep = getScrollStep(targetPosition);
-        var scrollHeight = getScrollHeight();
-        var targetY = scrollHeight - targetPosition;
-        var cosParameter = targetPosition / 2;
-        var scrollCount = 0;
-        var scrollMargin;
-        var pageHeight = getHeightOfEntirePage();
-        var heightOfWindow = getHeightOfWindow();
+        var targetPosition = getScrollTargetPosition(ele, offset);
+        var stepSize = getStepSize(targetPosition);
+        var initScrollHeight = window.pageYOffset;
+        var targetY = window.pageYOffset - targetPosition;
 
+        // Cancel on Wheel touch
         var scrollWheelTouched = false;
         var onWheelListener = function(e) {
             scrollWheelTouched = true;
             window.removeEventListener("wheel", onWheelListener);
         };
         window.addEventListener("wheel", onWheelListener);
-        // This is where the magic happens. A function is made depending
-        // on if the target postiion is below or above the current window position.
-        // From there, requestAnimationFrame does its magic of calling that
-        // step function over and over until it is close enough to the target
+
+        var stepCount = 0;
+        var scrollingFinished;
+
+
+        // Depending on if we are scrolling down or up,
+        // we create a scrollingFinished function to use as
+        // our conditional when doing the raf loop
         if (targetPosition < 0) {
-            var stepDownTo = function() {
-                // Checks to see if we hit the bottom
-                var atBottom = ((heightOfWindow + getScrollHeight()) === pageHeight);
-                // Need the - 5 due to the asymptote
-                if (getScrollHeight() < (targetY - 5) && !atBottom && !scrollWheelTouched) {
-                    scrollCount = scrollCount + 1;
-                    scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
-                    window.scrollTo(0, (scrollHeight - scrollMargin));
-                    requestAnimationFrame(stepDownTo);
-                }
-            };
-            requestAnimationFrame(stepDownTo);
+            scrollingFinished = function() {
+                return window.pageYOffset < (targetY - 5) && !scrollWheelTouched;
+            }
         } else {
-            var stepUpTo = function() {
-                // Need the + 5 due to the asymptote
-                if (getScrollHeight() > (targetY + 5) && !scrollWheelTouched) {
-                    scrollCount = scrollCount + 1;
-                    scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
-                    window.scrollTo(0, (scrollHeight - scrollMargin));
-                    requestAnimationFrame(stepUpTo);
-                }
-            };
-            requestAnimationFrame(stepUpTo);
+            scrollingFinished = function() {
+                return window.pageYOffset > (targetY + 5) && !scrollWheelTouched;
+            }
         }
+
+        // Where the loop actually starts
+        var step = function() {
+            // Need the - 5 due to the asymptote
+            if (scrollingFinished()) {
+                stepCount++;
+                window.scrollTo(0, (initScrollHeight - getScrollMargin(targetPosition, stepCount, stepSize)));
+                requestAnimationFrame(step);
+            }
+            //TODO Add a "onScrollFinished" callback
+        };
+        requestAnimationFrame(step);
+
     };
 
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
