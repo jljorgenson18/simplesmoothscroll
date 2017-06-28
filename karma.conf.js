0ec60923ab5ae96a9746a2dfbfebd7dd01de26ff
jljorgenson18 <jljorgenson18@gmail.com>
2017-06-28 16:34:22 -0500

    Removing Opera test reporter, adding a package-lock for npm v5

--- karma.conf.js
+++ karma.conf.js
@@ -1,49 +1,47 @@
 "use strict";
 
 module.exports = function(config) {
-    config.set({
-        frameworks: ['jasmine', 'fixture'],
-        files: [
-            "index.js",
-            "testing/helpers/**/*.js",
-            "testing/specs/**/*.js",
-            "testing/**/*.html"
-        ],
-        browsers: [
-            "Chrome",
-            "Firefox",
-            "Safari",
-            "Opera"
-        ],
-        preprocessors: {
-            // source files, that you wanna generate coverage for
-            // do not include tests or libraries
-            // (these files will be instrumented by Istanbul)
-            'index.js': ['coverage'],
-            "testing/**/*.html": ['html2js']
-        },
-        // coverage reporter generates the coverage
-        reporters: ['progress', 'coverage', 'kjhtml'],
-        coverageReporter: {
-            dir: 'coverage/',
-            reporters: [{
-                type: 'html',
-                subdir: 'html'
-            }, {
-                type: 'lcov',
-                subdir: 'lcov'
-            }]
-        },
-        plugins: [
-            'karma-fixture',
-            'karma-jasmine',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            "karma-opera-launcher",
-            "karma-safari-launcher",
-            'karma-jasmine-html-reporter',
-            'karma-coverage',
-            'karma-html2js-preprocessor'
-        ]
-    });
+  config.set({
+    frameworks: ['jasmine', 'fixture'],
+    files: [
+      "index.js",
+      "testing/helpers/**/*.js",
+      "testing/specs/**/*.js",
+      "testing/**/*.html"
+    ],
+    browsers: [
+      "Chrome",
+      "Firefox",
+      "Safari"
+    ],
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'index.js': ['coverage'],
+      "testing/**/*.html": ['html2js']
+    },
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage', 'kjhtml'],
+    coverageReporter: {
+      dir: 'coverage/',
+      reporters: [{
+        type: 'html',
+        subdir: 'html'
+      }, {
+        type: 'lcov',
+        subdir: 'lcov'
+      }]
+    },
+    plugins: [
+      'karma-fixture',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      "karma-safari-launcher",
+      'karma-jasmine-html-reporter',
+      'karma-coverage',
+      'karma-html2js-preprocessor'
+    ]
+  });
 };
